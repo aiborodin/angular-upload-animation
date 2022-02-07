b3f6144a7d42539bf230886e2a258d4b2e355241
alockylu <alockylu@gmail.com>
2022-02-07 16:56:58 +0200

    Lab 1 - Angular upload form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {animate, style, transition, trigger} from "@angular/animations";
+import {NgbProgressbar} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('uploadStart', [
+      transition(':enter', [
+        style({opacity: 0}),
+        animate('1s ease-out', style({opacity: 1}))
+      ]),
+      transition(':leave', animate('2s ease-out', style({opacity: 0})))
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'angular-upload-form';
+  @ViewChild('uploadProgress')
+  progressBar !: NgbProgressbar;
+
+  progress: number = 0;
+  completionLabel: string = '';
+
+  constructor() {
+  }
+
+  imitateFileUpload($event: any) {
+    this.progress += 4
+    setTimeout(() => {
+      this.progress += 12
+      setTimeout(() => {
+          this.progress += 44
+          setTimeout(() => {
+            this.progress += 40;
+            this.finishUpload()
+          }, 1000)
+        }
+        , 1000)
+    }, 1500)
+  }
+
+  finishUpload() {
+    this.progressBar.animated = false;
+    this.progressBar.striped = false;
+    this.progressBar.showValue = false;
+    this.completionLabel = 'Completed!';
+    setTimeout(() => this.progress = 0, 300);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
